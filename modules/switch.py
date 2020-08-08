1755a2e95f9b058f853f05b9a4be22c2d85be88a
John <60581076+JoHn-111@users.noreply.github.com>
2020-08-08 21:19:20 +0300

    Update switch.py

--- modules/switch.py
+++ modules/switch.py
@@ -1,6 +1,6 @@
 from pyrogram import Message, Filters
 from utils import app
-
+import time
 
 
 @app.on_message(Filters.command('sw', ['.']) & Filters.me)
@@ -8,12 +8,17 @@ def switch(client, message):
     text = ' '.join(message.command[1:])
     ru_keys = """ёйцукенгшщзхъфывапролджэячсмитьбю.Ё"№;%:?ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭ/ЯЧСМИТЬБЮ,"""
     en_keys = """`qwertyuiop[]asdfghjkl;'zxcvbnm,./~@#$%^&QWERTYUIOP{}ASDFGHJKL:"|ZXCVBNM<>?"""
-    if message.reply_to_message:
-        reply_text = message.reply_to_message.text
-        message.delete()
-        change = str.maketrans(ru_keys + en_keys, en_keys + ru_keys)
-        reply_text = str.translate(reply_text, change)
-        client.send_message(message.chat.id, reply_text)
+    if text == '':
+        if message.reply_to_message:
+            reply_text = message.reply_to_message.text
+            message.delete()
+            change = str.maketrans(ru_keys + en_keys, en_keys + ru_keys)
+            reply_text = str.translate(reply_text, change)
+            client.send_message(message.chat.id, reply_text)
+        else:
+            message.edit('No text for switch')
+            time.sleep(3)
+            message.delete()
     else:
         message.delete()
         change = str.maketrans(ru_keys + en_keys, en_keys + ru_keys)
