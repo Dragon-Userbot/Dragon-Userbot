5ea2395b377b269c1de50a9cb65c41428ca0a0db
John <60581076+JoHn-111@users.noreply.github.com>
2020-08-09 14:13:32 +0300

    Update text_to_img.py

--- modules/text_to_img.py
+++ modules/text_to_img.py
@@ -4,8 +4,7 @@ from utils import app
 import time
 
 
-
-@app.on_message(Filters.command('tti', ['.']))
+@app.on_message(Filters.command('tti', ['.']) & Filters.me)
 def text_to_img(client, message):
 	text_color = message.command[1]
 	background_color = message.command[2]
@@ -13,13 +12,14 @@ def text_to_img(client, message):
 	font_size = 20
 	message.delete()
 	print(text)
-	font = ImageFont.truetype("arial.ttf", 20)
+	font = ImageFont.truetype("modules/arial.ttf", 20)
 	width = font.getsize(max(text.split('\n'), key=len))[0] + 40
 	height = font.getsize(text.split('\n')[0])[1] * text.count('\n') + 40
-	font = ImageFont.truetype("arial.ttf", font_size)
+	font = ImageFont.truetype("modules/arial.ttf", font_size)
 	img = Image.new('RGB', (width, height), color=f'{background_color}')
 	draw = ImageDraw.Draw(img)
 	draw.text((10, 10), text, font=font, fill=f'{text_color}')
 	img.save('img.png')
 
 	client.send_photo(message.chat.id, 'img.png')
+
