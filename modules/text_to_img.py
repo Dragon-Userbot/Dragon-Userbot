35bf7af2ea3af76f6b4bbb26d4dcf692b768fe0a
John <60581076+JoHn-111@users.noreply.github.com>
2020-08-09 21:12:44 +0300

    Update text_to_img.py

--- modules/text_to_img.py
+++ modules/text_to_img.py
@@ -1,24 +1,30 @@
 from pyrogram import Message, Filters
-from PIL import Image, ImageDraw, ImageFont
 from utils import app
+
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
 import time
 
 
 @app.on_message(Filters.command('tti', ['.']) & Filters.me)
-def text_to_img(client, message):
+def text_2_img(client, message):
 	text_color = message.command[1]
 	background_color = message.command[2]
 	text = ' '.join(message.command[3:])
 	font_size = 20
 	message.delete()
 	print(text)
-	font = ImageFont.truetype("modules/arial.ttf", font_size)
-	width = font.getsize(max(text.split('\n'), key=len))[0] + 40
+	font = ImageFont.truetype("modules/arial.ttf", 20)
+	width = font.getsize(max(text.split('\n'), key=len))[0] + 35
 	height = font.getsize(text.split('\n')[0])[1] * text.count('\n') + 40
+	font = ImageFont.truetype("modules/arial.ttf", font_size)
 	img = Image.new('RGB', (width, height), color=f'{background_color}')
 	draw = ImageDraw.Draw(img)
 	draw.text((10, 10), text, font=font, fill=f'{text_color}')
-	img.save('img.png')
 
-	client.send_photo(message.chat.id, 'img.png')
+	tg_img = BytesIO()
+	tg_img.name = 'tg.png'
+	img.save(tg_img, 'PNG')
+	tg_img.seek(0)
 
+	client.send_photo(message.chat.id, tg_img)
