91b997a08f0bceab8325071cc082506f17f5e21b
John <60581076+JoHn-111@users.noreply.github.com>
2020-09-04 08:19:06 +0300

    Update repo.py

--- modules/repo.py
+++ modules/repo.py
@@ -1,7 +1,8 @@
-from pyrogram import Message, Filters
+from pyrogram.types import Message
+from pyrogram import filters
 from utils import app
 
 
-@app.on_message(Filters.command('repo', ['.']) & Filters.me)
+@app.on_message(filters.command('repo', ['.']) & filters.me)
 def repo(client, message):
     message.edit('My userbot code is <a href=https://github.com/JoHn-111/Userbot>here</a>')
