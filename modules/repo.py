17fec567195ce310423441e875d3661d65896d1e
John <60581076+JoHn-111@users.noreply.github.com>
2020-09-06 19:30:49 +0300

    Delete repo.py

--- modules/repo.py
+++
@@ -1,8 +0,0 @@
-from pyrogram.types import Message
-from pyrogram import filters
-from utils import app
-
-
-@app.on_message(filters.command('repo', ['.']) & filters.me)
-def repo(client, message):
-    message.edit('My userbot code is <a href=https://github.com/JoHn-111/Userbot>here</a>')
