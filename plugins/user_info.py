1503229fa269e39f1c615f646ce46b5d8a1c2425
John <60581076+JoHn-111@users.noreply.github.com>
2020-09-08 16:23:01 +0300

    Update user_info.py

--- plugins/user_info.py
+++ plugins/user_info.py
@@ -4,9 +4,13 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command('inf', ['.']) & filters.me)
 def get_user_inf(client, message):
-	user = message.reply_to_message.from_user.username
-	user_info = client.get_users(user)
-	user_info = (f'''|=<b>Username: @{user_info.username}
+    user = message.reply_to_message.from_user.id
+    user_info = client.get_users(user)
+    if user_info.username == None:
+        username = 'None'
+    else:
+        username = f'@{user_info.username}'
+    user_info = (f'''|=<b>Username: {username}
 |-Id: {user_info.id}
 |-Bot: {user_info.is_bot}
 |-Scam: {user_info.is_scam}
@@ -14,4 +18,4 @@ def get_user_inf(client, message):
 |-Status: {user_info.status}
 |-Deleted: {user_info.is_deleted}
 </b>''')
-	message.edit(user_info)
+    message.edit(user_info)
