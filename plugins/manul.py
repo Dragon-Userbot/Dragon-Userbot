fa4d284c6b2f2ee529a508fb4faef67f8980e8fc
John <60581076+JoHn-111@users.noreply.github.com>
2020-09-09 20:20:52 +0300

    Update manul.py

--- plugins/manul.py
+++ plugins/manul.py
@@ -1,13 +1,20 @@
 from pyrogram import Client, filters
+from .utils.utils import modules_help
+
 import time
 
 
 @Client.on_message(filters.command('manul', ['.']) & filters.me)
 def manul(client, message):
-	quantity = message.command[1]
-	quantity = int(quantity) + 1
-	print('manul')
-	message.delete()
-	for i in range(1, quantity):
-		client.send_message(message.chat.id, f"{i} манула(ов)")
-		time.sleep(0.2)
+    quantity = message.command[1]
+    quantity = int(quantity) + 1
+    print('manul')
+    message.delete()
+    for i in range(1, quantity):
+        client.send_message(message.chat.id, f"{i} манула(ов)")
+        time.sleep(0.2)
+
+
+modules_help.update({'manul': '''<b>Help for |manul|\nUsage:</b>
+<code>.manul [amount of manul]</code>
+<b>[Release manuls]</b>''', 'manul module': '<b>• Manul</b>:<code> manul</code>\n'})
