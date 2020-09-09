fab0bcd3eb8daa63cac5d5160abc1d5438622e3e
John <60581076+JoHn-111@users.noreply.github.com>
2020-09-09 20:39:46 +0300

    Update repo.py

--- plugins/repo.py
+++ plugins/repo.py
@@ -1,6 +1,13 @@
 from pyrogram import Client, filters
+from .utils.utils import modules_help
 
 
 @Client.on_message(filters.command('repo', ['.']) & filters.me)
 def repo(client, message):
-    message.edit('My userbot code is <a href=https://github.com/JoHn-111/Userbot>here</a>')
+    message.edit(
+        'My userbot code is <a href=https://github.com/JoHn-111/Userbot>here</a>')
+
+
+modules_help.update({'repo': '''<b>Help for |repo|\nUsage:</b>
+<code>.repo</code>
+<b>[Userbot code]</b>''', 'repo module': '<b>• Repo</b>:<code> repo</code>\n'})
