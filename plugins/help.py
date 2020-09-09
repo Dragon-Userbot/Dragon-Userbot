b543d5e118b68be38b7a19c45572d6a3b962def6
John <60581076+JoHn-111@users.noreply.github.com>
2020-09-09 20:13:27 +0300

    Add files via upload

---
+++ plugins/help.py
@@ -0,0 +1,24 @@
+from pyrogram import Client, filters
+from .utils.utils import modules_help
+
+
+@Client.on_message(filters.command('help', ['.']) & filters.me)
+def help(client, message):
+    module_name = ' '.join(message.command[1:])
+    help_message = '''<b>Help for Userbot\nFor more help on how to use a command, type <code>.help |module|</code>\n\nAvailable Modules:\n</b>'''
+    if module_name == '':
+        for modules, module_help in sorted(modules_help.items()):
+            if modules.endswith(' module'):
+                help_message += module_help
+        message.edit(help_message, parse_mode='html')
+    else:
+        try:
+            message.edit(modules_help[module_name.lower()])
+        except KeyError:
+            message.edit(
+                f'<b>Module <code>|{module_name}|</code> not found</b>')
+
+
+modules_help.update({'help': '''<b>Help for |help|\nUsage:</b>
+<code>.help |module name|</code>
+<b>[To get help]</b>''', 'help module': '<b>• Help</b>:<code> help</code>\n'})
