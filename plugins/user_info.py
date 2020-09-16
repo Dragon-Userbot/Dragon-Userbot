660dee75e46637316e74dd2acc3585b0c8d889c1
John <60581076+JoHn-111@users.noreply.github.com>
2020-09-16 20:14:10 +0300

    Update user_info.py

--- plugins/user_info.py
+++ plugins/user_info.py
@@ -1,28 +1,66 @@
 from pyrogram import Client, filters
 from .utils.utils import modules_help
+from pyrogram.raw import functions
 
 
 @Client.on_message(filters.command('inf', ['.']) & filters.me)
 def get_user_inf(client, message):
     user = message.reply_to_message.from_user.id
-    user_info = client.get_users(user)
-    if user_info.username == None:
+    user_info = client.send(
+        functions.users.GetFullUser(id=client.resolve_peer(user)))
+    if user_info.user.username == None:
         username = 'None'
     else:
-        username = f'@{user_info.username}'
+        username = f'@{user_info.user.username}'
+    if user_info.about == None:
+        about = 'None'
+    else:
+        about = user_info.about
     user_info = (f'''|=<b>Username: {username}
-|-Id: {user_info.id}
-|-Bot: {user_info.is_bot}
-|-Scam: {user_info.is_scam}
-|-Name: {user_info.first_name}
-|-Status: {user_info.status}
-|-Deleted: {user_info.is_deleted}
+|-Id: {user_info.user.id}
+|-Bot: {user_info.user.bot}
+|-Scam: {user_info.user.scam}
+|-Name: {user_info.user.first_name}
+|-Deleted: {user_info.user.deleted}
+|-BIO: {about}
 </b>''')
     message.edit(user_info)
 
 
+@Client.on_message(filters.command('inffull', ['.']) & filters.me)
+def get_full_user_inf(client, message):
+    user = message.reply_to_message.from_user.id
+    user_info = client.send(
+        functions.users.GetFullUser(id=client.resolve_peer(user)))
+    if user_info.user.username == None:
+        username = 'None'
+    else:
+        username = f'@{user_info.user.username}'
+    if user_info.about == None:
+        about = 'None'
+    else:
+        about = user_info.about
+    user_info = (f'''|=<b>Username: {username}
+|-Id: {user_info.user.id}
+|-Bot: {user_info.user.bot}
+|-Scam: {user_info.user.scam}
+|-Name: {user_info.user.first_name}
+|-Deleted: {user_info.user.deleted}
+|-BIO: {about}
+|-Contact: {user_info.user.contact}
+|-Can pin message: {user_info.can_pin_message}
+|-Mutual contact: {user_info.user.mutual_contact}
+|-Access hash: {user_info.user.access_hash}
+|-Restricted: {user_info.user.restricted}
+|-Verified: {user_info.user.verified}
+|-Phone calls available: {user_info.phone_calls_available}
+|-Phone calls private: {user_info.phone_calls_private}
+|-Blocked: {user_info.blocked}</b>''')
+    message.edit(user_info)
 
-modules_help.update({'user_info': '''<b>Help for |User_info|\nUsage:</b>
+
+modules_help.update({'user_info': '''<b>Help for |User info|\nUsage:</b>
 <code>.inf </code>
-<b>[Reply to any message from a user to find out information about him]</b>''', 'user_info module': '<b>• User_info</b>:<code> inf</code>\n'})
-
+<b>[Reply to any user message to find out brief information about him]</b>
+<code>.infull </code>
+<b>[Reply to any user message to find out full information about him]</b>''', 'user_info module': '<b>• User_info</b>:<code> inf, infull</code>\n'})
