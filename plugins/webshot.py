fd9179c206fd84033626b9fa75b58ad8a3804a42
John <60581076+JoHn-111@users.noreply.github.com>
2020-09-19 01:16:20 +0300

    Update webshot.py

--- plugins/webshot.py
+++ plugins/webshot.py
@@ -4,13 +4,13 @@ from .utils.utils import modules_help
 
 @Client.on_message(filters.command('webshot', ["."]) & filters.me)
 def webshot(client, message):
-    user_link = message.command[1]
-    message.delete()
-    full_link = f'https://webshot.deam.io/{user_link}/?width=1920&height=1080?type=png'
     try:
+        user_link = message.command[1]
+        message.delete()
+        full_link = f'https://webshot.deam.io/{user_link}/?width=1920&height=1080?type=png'
         client.send_document(message.chat.id, full_link, caption=f'{user_link}')
     except:
-        pass
+        client.send_message(message.chat.id, '<code>Something went wrong...</code>')
 
 
 modules_help.update({'webshot': '''<b>Help for |Webshot|\nUsage:</b>
