08210109eaa6ed8f1ef8cbfbfd8fcf24e474a713
John <60581076+JoHn-111@users.noreply.github.com>
2020-09-20 12:14:59 +0300

    Update utils.py

--- plugins/utils/utils.py
+++ plugins/utils/utils.py
@@ -1,1 +1,10 @@
+from sys import version_info
+
+
 modules_help = {}
+
+github = '<a href=https://github.com/JoHn-111/Userbot> github</a>'
+version = '0.1.5'
+license = '<a href=https://github.com/JoHn-111/Userbot/blob/master/LICENSE> Apache License 2.0</a>'
+copyright = '© <a href=https://github.com/JoHn-111>John</a>, 2020'
+python_version = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
