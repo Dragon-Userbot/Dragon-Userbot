6268317c8915a8dcd064c46ee92db64f7000c985
John <60581076+JoHn-111@users.noreply.github.com>
2020-09-30 19:50:54 +0300

    Update utils.py

--- plugins/utils/utils.py
+++ plugins/utils/utils.py
@@ -5,6 +5,6 @@ modules_help = {}
 
 github = '<a href=https://github.com/JoHn-111/Userbot> github</a>'
 version = '1.1.14'
-license = '<a href=https://github.com/JoHn-111/Userbot/blob/master/LICENSE> Apache License 2.0</a>'
+license = '<a href=https://github.com/JoHn-111/Userbot/blob/master/LICENSE> GNU General Public License v3.0</a>'
 copyright = '© <a href=https://github.com/JoHn-111>John</a>, 2020'
 python_version = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
