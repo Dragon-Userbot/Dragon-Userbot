2c8585ceeb1f55c39e04e1103ba580fd474602c2
John <60581076+JoHn-111@users.noreply.github.com>
2020-10-15 14:03:42 +0300

    Delete test.py

--- plugins/test.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import Client, filters
-from .utils.utils import modules_help
-
-
-@Client.on_message(filters.command('test', ['.']) & filters.me)
-def test_func(client, message):
-	message.edit('test')
