4307239bc860ea92c7a97714ae6d32e841c951ad
John <60581076+JoHn-111@users.noreply.github.com>
2020-10-18 17:42:52 +0300

    Add files via upload

---
+++ plugins/coronavirus.py
@@ -0,0 +1,45 @@
+from pyrogram import Client, filters
+from .utils.utils import modules_help
+from .utils.utils import requirements_list
+
+from covid import Covid
+
+@Client.on_message(filters.command('covid', ['.']) & filters.me)
+def covid_local(client, message):
+	region = ' '.join(message.command[1:])
+	covid = Covid(source="worldometers")
+	try:
+		local_status = covid.get_status_by_country_name(region)
+		message.edit("<b>=======🦠 COVID-19 STATUS 🦠=======</b>\n" +
+			f"<b>Region</b>: <code>{local_status['country']}</code>\n" +
+			"<b>====================================</b>\n" +
+			f"<b>🤧 New cases</b>: <code>{local_status['new_cases']}</code>\n" +
+			f"<b>😷 New deaths</b>: <code>{local_status['new_deaths']}</code>\n" +
+			"<b>====================================</b>\n" +
+			f"<b>😷 Сonfirmed</b>: <code>{local_status['confirmed']}</code>\n" +
+			f"<b>❗️ Active:</b> <code>{local_status['active']}</code>\n" +
+			f"<b>⚠️ Critical</b>: <code>{local_status['critical']}</code>\n" +
+			f"<b>💀 Deaths</b>: <code>{local_status['deaths']}</code>\n" +
+			f"<b>🚑 Recovered</b>: <code>{local_status['recovered']}</code>\n")
+	except ValueError:
+		message.edit(f'<code>There is no region called "{region}"</code>')
+
+
+@Client.on_message(filters.command('regions', ['.']) & filters.me)
+def regions(client, message):
+	countr = ''
+	covid = Covid(source="worldometers")
+	regions = covid.list_countries()
+	for region in regions:
+		region = f'{region}\n'
+		countr += region
+	message.edit(f'<code>{countr}</code>')
+
+
+modules_help.update({'covid': '''<b>Help for |Covid|\nUsage:</b>
+<code>.covid [region]</code>
+<b>[Status by region]</b>
+<code>.regions</code>
+<b>[Available regions]</b>''', 'covid module': '<b>• Covid</b>:<code> covid, regions</code>\n'})
+
+requirements_list.append('covid')
