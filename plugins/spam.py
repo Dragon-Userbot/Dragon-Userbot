e183e8d5bbb71bb6c2598fe1d572f890a97e0a54
John <60581076+JoHn-111@users.noreply.github.com>
2021-01-12 13:29:20 +0300

    Update spam.py

--- plugins/spam.py
+++ plugins/spam.py
@@ -4,6 +4,18 @@ from .utils.utils import modules_help
 import time
 
 
+@Client.on_message(filters.command('statspam', ['.']) & filters.me)
+def statspam(client, message):
+    quantity = message.command[1]
+    spam_text = ' '.join(message.command[2:])
+    quantity = int(quantity)
+    message.delete()
+    for i in range(quantity):
+        msg = client.send_message(message.chat.id, spam_text)
+        time.sleep(0.1)
+        msg.delete()
+        time.sleep(0.1)
+
 @Client.on_message(filters.command('spam', ['.']) & filters.me)
 def spam(client, message):
     quantity = message.command[1]
@@ -11,10 +23,12 @@ def spam(client, message):
     quantity = int(quantity)
     message.delete()
     for i in range(quantity):
-        client.send_message(message.chat.id, spam_text)
-        time.sleep(0.2)
-
+        msg = client.send_message(message.chat.id, spam_text)
+        time.sleep(0.15)
+        
 
 modules_help.update({'spam': '''<b>Help for |spam|\nUsage:</b>
 <code>.spam [amount of spam] [spam text]</code>
-<b>[Start spam]</b>''', 'spam module': '<b>• Spam</b>:<code> spam</code>\n'})
+<b>[Start spam]</b>
+<code>.statspam [amount of spam] [spam text]</code>
+<b>[Send and delete]</b>''', 'spam module': '<b>• Spam</b>:<code> spam, statspam</code>\n'})
