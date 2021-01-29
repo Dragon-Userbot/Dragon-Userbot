7fd673830b9e31dff1d200f536908504f9bada70
John <60581076+JoHn-111@users.noreply.github.com>
2021-01-29 21:57:53 +0300

    Update neko.py

--- plugins/neko.py
+++ plugins/neko.py
@@ -7,18 +7,18 @@ import time
 def getpic(query):
     return eval(requests.get(f"https://nekos.life/api/v2/img/{query}").text)['url']
 
-@Client.on_message(filters.command('neko', ['.']) & filters.me)
+@Client.on_message(filters.command(['neko', 'n'], ['.']) & filters.me)
 def neko(client, message):
     message.delete()
     query = message.command[1]
     client.send_message(message.chat.id, getpic(query), disable_web_page_preview=False)
 
-@Client.on_message(filters.command('neko_types', ['.']) & filters.me)
+@Client.on_message(filters.command(['neko_types', 'nt'], ['.']) & filters.me)
 def neko_types_func(client, message):
     n_t = '''<code>femdom tickle classic ngif erofeet meow erok poke les hololewd lewdk keta feetg nsfw_neko_gif eroyuri kiss 8ball kuni tits pussy_jpg cum_jpg pussy lewdkemo lizard slap lewd cum cuddle spank smallboobs goose Random_hentai_gif avatar fox_girl nsfw_avatar hug gecg boobs pat feet smug kemonomimi solog holo wallpaper bj woof yuri trap anal baka blowjob holoero feed neko gasm hentai futanari ero solo waifu pwankg eron erokemo</code>'''
     message.edit(n_t)
 
-@Client.on_message(filters.command('nekospam', ['.']) & filters.me)
+@Client.on_message(filters.command(['nekospam', 'ns'], ['.']) & filters.me)
 def neko_spam(client, message):
     message.delete()
     query = ' '.join(message.command[2:])
