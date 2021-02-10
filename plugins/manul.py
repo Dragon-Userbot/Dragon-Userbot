8f53ae60686d54efcd348292a0161cb197172fa8
John <60581076+JoHn-111@users.noreply.github.com>
2021-02-10 21:11:58 +0300

    async

--- plugins/manul.py
+++ plugins/manul.py
@@ -1,17 +1,16 @@
 from pyrogram import Client, filters
 from .utils.utils import modules_help
-
 import time
 
 
 @Client.on_message(filters.command('manul', ['.']) & filters.me)
-def manul(client, message):
+async def manul(client, message):
     quantity = message.command[1]
     quantity = int(quantity) + 1
     print('manul')
-    message.delete()
+    await message.delete()
     for i in range(1, quantity):
-        client.send_message(message.chat.id, f"{i} манула(ов)")
+        await client.send_message(message.chat.id, f"{i} манула(ов)")
         time.sleep(0.2)
 
 
