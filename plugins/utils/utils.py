1e34a836b341783d7f86a442e326ef72af4f7bb4
John <60581076+JoHn-111@users.noreply.github.com>
2021-02-10 21:51:45 +0300

    Update utils.py

--- plugins/utils/utils.py
+++ plugins/utils/utils.py
@@ -4,7 +4,7 @@ from sys import version_info
 modules_help = {}
 requirements_list = []
 
-github = '<a href=https://github.com/JoHn-111/Userbot> github</a>'
-license = '<a href=https://github.com/JoHn-111/Userbot/blob/master/LICENSE> GNU General Public License v3.0</a>'
+github = '<a href=https://github.com/JoHn-111/Dragon-Userbot> github</a>'
+license = '<a href=https://github.com/JoHn-111/Dragon-Userbot/blob/master/LICENSE> GNU General Public License v3.0</a>'
 copyright = '© <a href=https://github.com/JoHn-111>John</a>, 2020'
 python_version = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
