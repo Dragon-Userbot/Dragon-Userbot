16248792cc3f3378ab6512a17012bd7ebe96e76b
John <60581076+JoHn-111@users.noreply.github.com>
2021-02-10 21:15:42 +0300

    async

--- plugins/utils/scripts.py
+++ plugins/utils/scripts.py
@@ -4,6 +4,6 @@ from pyrogram import Client, filters
 date_dict = {}
 
 @Client.on_message(filters.chat("@creationdatebot"), group=-1)
-def get_date(client, message):
-    client.read_history("@creationdatebot")
+async def get_date(client, message):
+    await client.read_history("@creationdatebot")
     date_dict.update({"date": message.text})
