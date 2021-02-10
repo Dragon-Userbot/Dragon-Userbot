0c3d0bf920a5bf2f61e71cfd9881a1c378cf2d79
John <60581076+JoHn-111@users.noreply.github.com>
2021-02-10 21:06:35 +0300

    Update afk.py

--- plugins/afk.py
+++ plugins/afk.py
@@ -6,24 +6,24 @@ import time
 
 
 @Client.on_message(filters.command('afk', ['.']) & filters.me)
-def afk(client, message):
+async def afk(client, message):
     global start, end
     start = datetime.datetime.now().replace(microsecond=0)
-    message.edit("<b>I'm going afk</b>")
+    await message.edit("<b>I'm going afk</b>")
 
     
 @Client.on_message(filters.command('unafk', ['.']) & filters.me)
-def unafk(client, message):
+async def unafk(client, message):
     try:
         global start, end
         end = datetime.datetime.now().replace(microsecond=0)
         afk_time = (end - start)
-        message.edit(f"<b>I'm not AFK anymore.\nI was afk {afk_time}</b>")
+        await message.edit(f"<b>I'm not AFK anymore.\nI was afk {afk_time}</b>")
         print(afk_time)
     except NameError:
-        message.edit("<b>You weren't afk</b>")
+        await message.edit("<b>You weren't afk</b>")
         time.sleep(3)
-        message.delete()
+        await message.delete()
 
 
 modules_help.update({'afk': '''<b>Help for |afk|\nUsage:</b>
