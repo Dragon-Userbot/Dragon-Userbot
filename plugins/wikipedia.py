02a9084f394c7783e04f151830307dc830a7cc05
John <60581076+JoHn-111@users.noreply.github.com>
2021-02-10 21:13:27 +0300

    acync

--- plugins/wikipedia.py
+++ plugins/wikipedia.py
@@ -5,24 +5,24 @@ from .utils.utils import requirements_list
 
 
 @Client.on_message(filters.command('wiki', ['.']) & filters.me)
-def wiki(client, message):
+async def wiki(client, message):
 	lang = message.command[1]
 	user_request = ' '.join(message.command[2:])
 	if user_request == '':
-		wikipedia.set_lang("en")
+		wikipedia.set_lang("ru")
 		user_request = ' '.join(message.command[1:])
 	try:
-		if lang == '-ru':
-			wikipedia.set_lang("ru")
+		if lang == 'en':
+			wikipedia.set_lang("en")
 
 		result = wikipedia.summary(user_request)
-		message.edit(f'''<b>Request:</b>
+		await message.edit(f'''<b>Request:</b>
 <code>{user_request}</code>
 <b>Result:</b>
 <code>{result}</code>''')
 
-	except Exception as exc:
-		message.edit(f'''<b>Request:</b>
+	except:
+		await message.edit(f'''<b>Request:</b>
 <code>{user_request}</code>
 <b>Result:</b>
 <code>{exc}</code>''')
@@ -31,7 +31,7 @@ def wiki(client, message):
 modules_help.update({'wikipedia': '''<b>Help for |Wikipedia|\nUsage:</b>
 <code>.wiki [request]</code>
 <b>[Search on the English Wikipedia]</b>
-<code>.wiki -ru [request]</code>
+<code>.wiki ru [request]</code>
 <b>[Search in Russian Wikipedia]</b>''', 'wikipedia module': '<b>• Wikipedia</b>:<code> wiki</code>\n'})
 
 requirements_list.append('wikipedia')
