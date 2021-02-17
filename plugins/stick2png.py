43a888c477a899f5988f3697cd243273a19136c0
John <60581076+JoHn-111@users.noreply.github.com>
2021-02-17 22:10:33 +0300

    Update stick2png.py

--- plugins/stick2png.py
+++ plugins/stick2png.py
@@ -1,1 +1,31 @@
+from pyrogram import Client, filters
+from .utils.utils import modules_help
+import os
+import time
+
+
+@Client.on_message(filters.command(['stick2png', 'stp'], ['.']) & filters.me)
+async def stick2png(client, message):
+    if message.reply_to_message:
+        reply = message.reply_to_message
+        if reply.sticker:
+            if not reply.sticker.is_animated:
+                await message.edit('<code>Picture processing...</code>')
+                file = await client.download_media(message.reply_to_message, 'out.png')
+                await client.send_photo(message.chat.id, 'downloads/out.png', reply_to_message_id=message.reply_to_message.message_id)
+                await message.delete()
+                os.remove('downloads/out.png')
+            else:
+                msg = await message.edit('<b>Stiker is animated!</b>')
+                time.sleep(3)
+                await msg.delete()    
+        else:
+            msg = await message.edit('<b>Reply to stiker!</b>')
+            time.sleep(3)
+            await msg.delete()
+
+
+modules_help.update({'stick2png': '''<b>Help for |Stick to png|\nUsage:</b>
+<code>.stp </code>
+<b>[Reply to stiker]</b>''', 'stick2png module': '<b>• Stick2png</b>:<code> stick2png, stp</code>\n'})
 
