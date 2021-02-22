0273cf1bc13ccc7b85429dcf5ea9155c5ad10542
John <60581076+JoHn-111@users.noreply.github.com>
2021-02-22 22:16:14 +0300

    Update tagall.py

--- plugins/tagall.py
+++ plugins/tagall.py
@@ -1,1 +1,31 @@
+from pyrogram import Client, filters
+from .utils.utils import modules_help
+import asyncio
+
+
+@Client.on_message(filters.command('tagall', ['.']) & filters.me)
+async def tagall(client, message):
+	await message.delete()
+	chat_id = message.chat.id
+	string = ""
+	limit = 1
+	icm = client.iter_chat_members(chat_id)
+	async for member in icm:
+		tag = member.user.username
+		if limit <= 5:
+			if tag != None:
+				string += f"@{tag}\n"
+			else:
+				string += f"{member.user.mention}\n"
+			limit += 1
+		else:
+			await client.send_message(chat_id, text=string)
+			limit = 1
+			string = ""
+			await asyncio.sleep(2.5)
+
+
+modules_help.update({'tagall': '''<b>Help for |tagall|\nUsage:</b>
+<code>.tagall</code>
+<b>[Tag all members]</b>''', 'tagall module': '<b>• Tagall</b>:<code> tagall</code>\n'})
 
