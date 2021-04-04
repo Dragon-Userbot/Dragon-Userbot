796fdf6a43427a98dffaea8977e12777eed6d75b
John <60581076+JoHn-111@users.noreply.github.com>
2021-04-04 11:50:27 +0300

    Add files via upload

---
+++ plugins/weather.py
@@ -0,0 +1,20 @@
+#rewrote the module from @ Fl1yd
+
+from pyrogram import Client, filters
+from .utils.utils import modules_help
+
+import requests
+
+
+@Client.on_message(filters.command('weather', ['.']) & filters.me)
+async def weather(client, message):
+    city = message.command[1]
+    await message.edit("<code>Processing the request...</code>")
+    r = requests.get(f"https://wttr.in/{city}?0?q?T&lang=en")
+    await message.edit(f"<code>City: {r.text}</code>")
+
+    
+modules_help.update({'weather': '''<b>Help for |weather|\nUsage:</b>
+<code>.weather [city]</code>
+<b>[Get the weather in the selected city]</b>''', 'weather module': '<b>• Weather</b>:<code> weather</code>\n'})
+
