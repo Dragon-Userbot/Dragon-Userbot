b7c1863dd3ca45b39a1bdeba11da46f9b07195b0
John <60581076+JoHn-111@users.noreply.github.com>
2021-05-15 23:51:29 +0300

    Update updater.py

--- plugins/updater.py
+++ plugins/updater.py
@@ -7,12 +7,12 @@ import asyncio
 
 
 async def restart(client: Client, message: Message):
-    await client.restart(block=False)
+    await client.restart()
     await message.edit('<code>Restart was successful!</code>')
     
     
 async def update_restart(client: Client, message: Message):
-    await client.restart(block=False)
+    await client.restart()
     await message.edit('<code>Restart was successful!</code>')
     await asyncio.sleep(3)
     await message.edit('<code>Update process completed!</code>')
