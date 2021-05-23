747fa8762f33ae0ad6c777283e70a5a1df78e6e7
John <60581076+john-phonk@users.noreply.github.com>
2021-05-23 20:04:46 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from pyrogram import Client
+from pyrogram import Client, idle
+import sys
 
 
 app = Client("my_account")
 
 if __name__ == '__main__':
-    app.run()
+    app.start()
+    if len(sys.argv) == 4:
+        try:
+            restart_type = sys.argv[3]
+            if restart_type == '1': text = '<code>Update process completed!</code>'
+            else: text = '<code>Restart completed!</code>'
+            app.send_message(
+                chat_id=sys.argv[1], text=text, reply_to_message_id=int(sys.argv[2]))
+        except:
+            app.send_message(
+                chat_id=sys.argv[1], text=text)
+    idle()
