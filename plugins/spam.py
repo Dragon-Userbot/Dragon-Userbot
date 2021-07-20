503639ce87b1bf9153d7ebffaa425fa5f3846a1f
Medovyha <61845043+Medovyha@users.noreply.github.com>
2021-07-20 23:01:59 +0300

    Added reply option for spam
    Now, if a spam command is replied to a message, all the spam messages will be also replied to the same message

--- plugins/spam.py
+++ plugins/spam.py
@@ -24,8 +24,17 @@ async def spam(client: Client, message: Message):
     spam_text = ' '.join(message.command[2:])
     quantity = int(quantity)
     await message.delete()
+
+    if message.reply_to_message:
+        reply_to_id = message.reply_to_message.message_id
+        for _ in range(quantity):
+            await client.send_message(message.chat.id, spam_text,
+                                      reply_to_message_id=reply_to_id)
+            await asyncio.sleep(0.15)
+        return
+
     for i in range(quantity):
-        msg = await client.send_message(message.chat.id, spam_text)
+        await client.send_message(message.chat.id, spam_text)
         await asyncio.sleep(0.15)
 
 
@@ -35,6 +44,15 @@ async def fastspam(client: Client, message: Message):
     spam_text = ' '.join(message.command[2:])
     quantity = int(quantity)
     await message.delete()
+    
+    if message.reply_to_message:
+        reply_to_id = message.reply_to_message.message_id
+        for _ in range(quantity):
+            await client.send_message(message.chat.id, spam_text,
+                                      reply_to_message_id=reply_to_id)
+            await asyncio.sleep(0.02)
+        return
+    
     for i in range(quantity):
         msg = await client.send_message(message.chat.id, spam_text)
         await asyncio.sleep(0.02)
@@ -46,6 +64,15 @@ async def slowspam(client: Client, message: Message):
     spam_text = ' '.join(message.command[2:])
     quantity = int(quantity)
     await message.delete()
+
+    if message.reply_to_message:
+        reply_to_id = message.reply_to_message.message_id
+        for _ in range(quantity):
+            await client.send_message(message.chat.id, spam_text,
+                                      reply_to_message_id=reply_to_id)
+            await asyncio.sleep(0.9)
+        return
+
     for i in range(quantity):
         msg = await client.send_message(message.chat.id, spam_text)
         await asyncio.sleep(0.9)
