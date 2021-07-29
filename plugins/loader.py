158fb81d31e60073829878b0e248fba626b47acf
AlbertDev <63642425+zvam1@users.noreply.github.com>
2021-07-29 14:22:53 +0400

    Add new download features (loader module)
    For https://github.com/Dragon-Userbot/Dragon-Userbot/pull/9

--- plugins/loader.py
+++ plugins/loader.py
@@ -4,25 +4,58 @@ from .utils.utils import modules_help, prefix
 from .utils.scripts import restart
 import requests
 import os
+import hashlib
+
+
+@Client.on_message(filters.command(['modhash', 'mh'], prefix) & filters.me)
+async def get_mod_hash(client: Client, message: Message):
+    if len(message.command) == 1:
+        return
+    url = message.command[1]
+    resp = requests.get(url)
+    if not resp.ok:
+        await message.edit(f'<b>Troubleshooting with downloading module <code>{url}</code></b>')
+        return
+    await message.edit(f'<b>Module hash: <code>{hashlib.sha256(resp.content).hexdigest()}</code></b>')
 
 
 @Client.on_message(filters.command(['loadmod', 'lm'], prefix) & filters.me)
 async def load_mods(client: Client, message: Message):
-    if len(message.command) > 1:
-        url = message.command[1]
-        if '/'.join(url.split('/')[:6]) == 'https://raw.githubusercontent.com/Dragon-Userbot/custom_modules/main':
-            if not os.path.exists(f'{os.path.abspath(os.getcwd())}/plugins/custom_modules'):
-                os.mkdir(
-                    f'{os.path.abspath(os.getcwd())}/plugins/custom_modules')
+    if len(message.command) == 1:
+        return
+    url = message.command[1]
 
-            with open(f'./plugins/custom_modules/{url.split("/")[6]}', 'wb') as mod:
-                code = requests.get(url)
-                mod.write(code.content)
-            await message.edit(f'<b>The module <code>{url.split("/")[6].split(".")[0]}</code> is loaded!</b>')
-            await restart()
+    async def download_mod(content=None):
+        if not os.path.exists(f'{os.path.abspath(os.getcwd())}/plugins/custom_modules'):
+            os.mkdir(f'{os.path.abspath(os.getcwd())}/plugins/custom_modules')
+        code = requests.get(url) if content is None else content
+        if not code.ok:
+            await message.edit(f'<b>Can\'t find module <code>{url.split("/")[-1].split(".")[0]}</code></b>')
+            return
+        with open(f'./plugins/custom_modules/{url.split("/")[-1]}', 'wb') as mod:
+            mod.write(code.content)
+        await message.edit(f'<b>The module <code>{url.split("/")[-1].split(".")[0]}</code> is loaded!</b>')
+        await restart()
+
+    if '/'.join(url.split('/')[:6]) == 'https://raw.githubusercontent.com/Dragon-Userbot/custom_modules/main':
+        await download_mod()
+    elif '/' not in url and '.' not in url:
+        url = f'https://raw.githubusercontent.com/Dragon-Userbot/custom_modules/main/{url}.py'
+        await download_mod()
+    else:
+        resp = requests.get(url)
+        if not resp.ok:
+            await message.edit(f'<b>Troubleshooting with downloading module <code>{url}</code></b>')
+            return
+        hashes = requests.get('https://raw.githubusercontent.com/Dragon-Userbot/custom_modules/main/modules_hashes.txt').text
+        if hashlib.sha256(resp.content).hexdigest() in hashes:
+            await download_mod(resp)
         else:
-            await message.edit('<b>Only modules from the official <a href=https://github.com/Dragon-Userbot/custom_modules>custom_modules</a> repository are supported!</b>',
-                               disable_web_page_preview=True)
+            await message.edit(
+                '<b>Only <a href=https://github.com/Dragon-Userbot/custom_modules/main/modules_hashes.txt>verified'
+                '</a> modules or from the official <a href=https://github.com/Dragon-Userbot/custom_modules>'
+                'custom_modules</a> repository are supported!</b>',
+                disable_web_page_preview=True)
 
 
 @Client.on_message(filters.command(['unloadmod', 'ulm'], prefix) & filters.me)
@@ -47,5 +80,6 @@ async def unload_mods(client: Client, message: Message):
 
 modules_help.update(
     {'loader': '''loadmod [link] - Download module]\n[Only modules from the official custom_modules repository are supported, 
-                  unloadmod [module_name] - Delete module''',
-    'loader module': 'Loader: loadmod, unloadmod'})
+                  unloadmod [module_name] - Delete module,
+                  modhash [link] - Get module hash by link''',
+    'loader module': 'Loader: loadmod, unloadmod, modhash'})
