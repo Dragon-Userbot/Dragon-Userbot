c4c3ce053c037a3a25abc0eb1d9859d7aec55667
John <60581076+john-phonk@users.noreply.github.com>
2021-08-01 21:26:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ apt-get install python3 -y
 pkg install python -y
 pip3 install wheel
 pip3 install --upgrade pip
-pkg install python libjpeg-turbo libcrypt ndk-sysroot clang zlib
+pkg install python libjpeg-turbo libcrypt ndk-sysroot clang zlib -y
 LDFLAGS="-L${PREFIX}/lib/" CFLAGS="-I${PREFIX}/include/" pip install --upgrade wheel pillow
 apt install ffmpeg -y
 pip3 install -r requirements.txt
