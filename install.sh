16a52ce8c3b6479edeb778839b00ffb26f64387f
fuccsoc <fuccsoc@gmail.com>
2021-11-08 16:04:49 +0300

    First DB implementation Install script optimization

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@ apt-get update && apt-get upgrade -y
 apt install python3-pip git wget -y
 apt-get install python3 -y
 pkg install python -y
-pip3 install wheel
 pip3 install --upgrade pip
 pkg install python libjpeg-turbo libcrypt ndk-sysroot clang zlib -y
 LDFLAGS="-L${PREFIX}/lib/" CFLAGS="-I${PREFIX}/include/" pip install --upgrade wheel pillow
@@ -14,4 +13,3 @@ read -p 'Enter your db_url: ' uservar
 echo ====================================
 
 python3 install.py $uservar
-
---
+++ plugins/utils/db.py
@@ -0,0 +1,33 @@
+import motor.motor_asyncio as m
+import configparser
+import os
+import sys
+
+class DataBase():
+    def __init__(self):
+        config_path = os.path.join(sys.path[0], 'config.ini')
+        config = configparser.ConfigParser()
+        config.read(config_path)
+        db_url = config.get("pyrogram", "db_url")
+        self._DB = m.AsyncIOMotorClient(db_url).Dragon_Userbot
+    
+    async def set(self, module: str, variable: str, value):
+        modcollection = self._DB[module] 
+        doc = (await modcollection.find_one({'var': variable}))
+        if doc == None:
+            res = (await modcollection.insert_one({'var': variable, 'val': value}))
+            doc = (await modcollection.find_one({'_id': res.inserted_id}))
+        else:
+            res = (await modcollection.replace_one(doc, {'var': variable, 'val': value}))
+            doc = (await modcollection.find_one({'var':variable}))
+        return {doc['var']: doc['val']}
+    
+    async def get(self, module: str, variable: str, expected_value = None):
+        modcollection = self._DB[module]
+        doc = (await modcollection.find_one({'var': variable}))
+        if doc == None:
+            return expected_value
+        else:
+            return doc['val']
+
+db = DataBase()
