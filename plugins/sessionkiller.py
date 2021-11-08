f3a63a14b3917fcf5f54ae166156ff1085a21275
Albert <63642425+iamnalinor@users.noreply.github.com>
2021-11-09 02:16:52 +0400

    module fix & new database support (requires test)

--- plugins/sessionkiller.py
+++ plugins/sessionkiller.py
@@ -1,82 +1,86 @@
 from pyrogram import Client, filters
+from pyrogram.errors import RPCError
 from pyrogram.types import Message
 from pyrogram.raw.functions.account import GetAuthorizations, ResetAuthorization
 from pyrogram.raw.types import UpdateServiceNotification
 from pyrogram import ContinuePropagation
-from .utils.utils import modules_help, prefix, sessionkiller_enabled, config_path, config
+from .utils.utils import modules_help, prefix
 from .utils.scripts import restart
 from html import escape
 from datetime import datetime
+from main import app
 import time
 
+with app:
+    auths = app.send(GetAuthorizations())['authorizations']
+    auths_hashs = []
+    for auth in auths:
+        auths_hashs.append(auth['hash'])
+
+
 @Client.on_message(filters.command(['sessionkiller', 'sk'], prefix) & filters.me)
-async def sessionkiller(client: Client, message: Message):
+async def sessionkiller(_, message: Message):
     if len(message.command) == 1:
-        if sessionkiller_enabled:
+        if db.get('sessionkiller', 'enabled', False):
             await message.edit('Sessionkiller status: <b>enabled</b>\n'
                                f'You can disable it with <code>{prefix}sessionkiller disable</code>')
         else:
             await message.edit('Sessionkiller status: <b>disabled</b>\n'
                                f'You can enable it with <code>{prefix}sessionkiller enable</code>')
     else:
-        if message.command[1] in ['enable', 'on', '1', 'yes']:
-            config.set('sessionkiller', 'enabled', '1')
-            with open(config_path, "w") as config_file:
-                config.write(config_file)
+        if message.command[1] in ['enable', 'on', '1', 'yes', 'true']:
+            db.set('sessionkiller', 'enabled', True)
             await message.edit(f'<b>Sessionkiller enabled!</b>')
-            await restart()
-        elif message.command[1] in ['disable', 'off', '0', 'no']:
-            config.set('sessionkiller', 'enabled', '0')
-            with open(config_path, "w") as config_file:
-                config.write(config_file)
+        elif message.command[1] in ['disable', 'off', '0', 'no', 'false']:
+            db.set('sessionkiller', 'enabled', False)
             await message.edit(f'<b>Sessionkiller disabled!</b>')
-            await restart()
         else:
             await message.edit(f'<b>Usage: {prefix}sessionkiller [enable|disable]</b>')
 
 
 @Client.on_raw_update()
 async def check_new_login(client: Client, update: UpdateServiceNotification, _, __):
-    if not sessionkiller_enabled:
-        raise ContinuePropagation
     if not isinstance(update, UpdateServiceNotification) or \
             not update.type.startswith('auth'):
+        raise ContinuePropagation
+    if not db.get('sessionkiller', 'enabled', False):
         raise ContinuePropagation
     authorizations = (await client.send(GetAuthorizations()))['authorizations']
     for auth in authorizations:
         if auth.current:
             continue
-        if (time.time() - auth.date_created) < 3:
+        if auth['hash'] not in auths_hashs:
             # found new unexpected login
             try:
                 await client.send(ResetAuthorization(hash=auth.hash))
-            except:
-                info_text = "Someone tried to log in to your account. You can see this report because " \
+            except RPCError:
+                info_text = "Someone tried to log in to your account. You can see this report because you" \
                             "turned on this feature. But I couldn't terminate attacker's session and " \
                             "⚠ <b>you must reset it manually</b>. You should change your 2FA password " \
-                            "(if enabled), or setup it.\n"
+                            "(if enabled), or set it.\n"
             else:
                 info_text = "Someone tried to log in to your account. Since you have enabled " \
-                            "this feature, we have deleted the attacker's session from this account. " \
-                            "You should change your 2FA password (if enabled), or setup it.\n"
+                            "this feature, I deleted the attacker's session from your account. " \
+                            "You should change your 2FA password (if enabled), or set it.\n"
             logined_time = datetime.utcfromtimestamp(auth.date_created).strftime('%d-%m-%Y %H-%M-%S UTC')
-            data = '<b>!!! ACTION REQUIRED !!!</b>\n' \
-                   + info_text + \
-                   "Below is the information about the attacker that I got.\n\n" \
-                   f'Unique authorization hash: <code>{auth.hash}</code> (not valid anymore)\n' \
-                   f'Device model: <code>{escape(auth.device_model)}</code>\n' \
-                   f'Platform: <code>{escape(auth.platform)}</code>\n' \
-                   f'API ID: <code>{auth.api_id}</code>\n' \
-                   f'App name: <code>{escape(auth.app_name)}</code>\n' \
-                   f'App version: <code>{auth.app_version}</code>\n' \
-                   f'Logined at: <code>{logined_time}</code>\n' \
-                   f'IP: <code>{auth.ip}</code>\n' \
-                   f'Country: <code>{auth.country}</code>\n' \
-                   f'Official app: <b>{"yes" if auth.official_app else "no"}</b>\n\n' \
-                   f'<b>It is you? Type <code>{prefix}sessionkiller disable</code> and try logging ' \
-                   f'in again.</b>'
-            me = await client.get_me()
-            await client.send_message(me.id, data)  # sending report to saved messages
+            full_report = '<b>!!! ACTION REQUIRED !!!</b>\n' \
+                          + info_text + \
+                          "Below is the information about the attacker that I got.\n\n" \
+                          f'Unique authorization hash: <code>{auth.hash}</code> (not valid anymore)\n' \
+                          f'Device model: <code>{escape(auth.device_model)}</code>\n' \
+                          f'Platform: <code>{escape(auth.platform)}</code>\n' \
+                          f'API ID: <code>{auth.api_id}</code>\n' \
+                          f'App name: <code>{escape(auth.app_name)}</code>\n' \
+                          f'App version: <code>{auth.app_version}</code>\n' \
+                          f'Logined at: <code>{logined_time}</code>\n' \
+                          f'IP: <code>{auth.ip}</code>\n' \
+                          f'Country: <code>{auth.country}</code>\n' \
+                          f'Official app: <b>{"yes" if auth.official_app else "no"}</b>\n\n' \
+                          f'<b>It is you? Type <code>{prefix}sk off</code> and try logging ' \
+                          f'in again.</b>'
+            # schedule sending report message so user will get notification
+            schedule_date = int(time.time() + 5)
+            await client.send_message('me', full_report, schedule_date=schedule_date)
             return
 
 
