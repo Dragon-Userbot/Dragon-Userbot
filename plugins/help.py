fcd149050fc9cdcb4a0115d199619aafd4351fff
fuccsoc <fuccsoc@gmail.com>
2021-11-28 16:44:02 +0300

    Prefix fix

--- plugins/help.py
+++ plugins/help.py
@@ -9,7 +9,7 @@ async def help(client, message: Message):
     module_name = " ".join(message.command[1:])
     if module_name == "":
         messages = [
-            """<b>Help for <a href="https://t.me/Dragon_Userbot_chat">Dragon-Userbot</a></b>\n<b>For more help on how to use a command, type </b> <code>{prefix}help [module]</code>\n\n<b>Available Modules:</b>\n"""
+            f"""<b>Help for <a href="https://t.me/Dragon_Userbot_chat">Dragon-Userbot</a></b>\n<b>For more help on how to use a command, type </b> <code>{prefix}help [module]</code>\n\n<b>Available Modules:</b>\n"""
         ]
         msg_cnt = 0
         for mod in modules_help:
