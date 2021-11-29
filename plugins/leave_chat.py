b42acc06adb6d9066c023c3ba2159620e1fa4f8a
John <60581076+john-phonk@users.noreply.github.com>
2021-11-29 17:45:36 +0300

    Update leave_chat.py
    fix by @THE_BURGERNET777

--- plugins/leave_chat.py
+++ plugins/leave_chat.py
@@ -4,11 +4,14 @@ from .utils.utils import modules_help, prefix
 import asyncio
 
 
-@Client.on_message(filters.command(["leave_chat"], prefix) & filters.me)
+@Client.on_message(filters.command(["leave_chat", "lc"], prefix) & filters.me)
 async def leave_chat(client: Client, message: Message):
-    m = await message.edit("<code>Goodbye...</code>")
-    await asyncio.sleep(3)
-    await client.leave_chat(chat_id=message.chat.id)
+    if message.chat.type in ["group", "supergroup"]:
+        await message.edit("<code>Goodbye...</code>")
+        await asyncio.sleep(3)
+        await client.leave_chat(chat_id=message.chat.id)
+    else:
+        await message.edit("This is not a group/suppergroup")
 
 
 modules_help.append({"leave_chat": [{"leave_chat": "Quit chat"}]})
