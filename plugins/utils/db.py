eb312e21764f2501a5df20d92b53b8ba3dcf1ae2
John <60581076+john-phonk@users.noreply.github.com>
2021-12-05 21:50:40 +0300

    Update db.py

--- plugins/utils/db.py
+++ plugins/utils/db.py
@@ -2,6 +2,10 @@ import pymongo as md
 import configparser
 import os
 import sys
+import dns.resolver
+
+dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
+dns.resolver.default_resolver.nameservers = ["8.8.8.8"]
 
 
 class DataBase:
