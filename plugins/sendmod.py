83bda382bd3a851cedccd3865b2339243d3be756
fuccsoc <fuccsoc@gmail.com>
2021-12-06 09:08:03 +0300

    Sendmod fixed (#38)

--- plugins/sendmod.py
+++ plugins/sendmod.py
@@ -10,21 +10,28 @@ import asyncio
 
 @Client.on_message(filters.command(["sendmod", "sm"], prefix) & filters.me)
 async def sendmod(client: Client, message: Message):
-    mod_name = message.command[1]
+    module_name = message.command[1]
     try:
         await message.edit("<code>Dispatch...</code>")
-        text = help_formatting(
-            modules_help[mod_name.lower()],
-            help_type="one_mod",
-            module_name=mod_name.lower(),
-        )
-        if os.path.isfile(f"plugins/{mod_name}.py"):
+        text = f"<b>Help for <i>{module_name}</i>\n\nUsage:</b>\n"
+        found = False
+        for mh in modules_help:
+            if list(mh.keys())[0].lower() == module_name.lower():
+                found = True
+                cmds = list(mh.values()).pop(0)
+                for u_cmd in cmds:
+                    cmd = list(u_cmd.items())[0]
+                    text += f"""<code>{prefix + cmd[0]}</code> - <i>{cmd[1]}</i>\n"""
+        if not found:
+            text = "<b>Module <i>{module_name}</i> not found!</b>"
+
+        if os.path.isfile(f"plugins/{module_name.lower()}.py"):
             await client.send_document(
-                message.chat.id, f"plugins/{mod_name}.py", caption=text
+                message.chat.id, f"plugins/{module_name.lower()}.py", caption=text
             )
-        elif os.path.isfile(f"plugins/custom_modules/{mod_name}.py"):
+        elif os.path.isfile(f"plugins/custom_modules/{module_name.lower()}.py"):
             await client.send_document(
-                message.chat.id, f"plugins/custom_modules/{mod_name}.py", caption=text
+                message.chat.id, f"plugins/custom_modules/{module_name.lower()}.py", caption=text
             )
         await message.delete()
     except:
