e76a9a66d4dffae63105abd3bbcf29cdf5d47a10
TupaBan-Dev <78302942+TupaBan-Dev@users.noreply.github.com>
2022-01-09 01:43:52 +0300

    Fix UserInfo

--- plugins/user_info.py
+++ plugins/user_info.py
@@ -9,7 +9,7 @@ from .utils.utils import modules_help, prefix
 
 
 @Client.on_message(filters.command("inf", prefix) & filters.me)
-async def get_user_inf(client: Client, message: Message):
+async def get_user_inf(client, message):
     if len(message.text.split()) >= 2:
         try:
             user = await client.get_users(message.text.split()[1])
@@ -27,24 +27,24 @@ async def get_user_inf(client: Client, message: Message):
     user_info = await client.send(
         functions.users.GetFullUser(id=await client.resolve_peer(user))
     )
-    if user_info.user.username is None:
+    if user_info.users[0].username is None:
         username = "None"
     else:
-        username = f"@{user_info.user.username}"
-    about = "None" if user_info.about is None else user_info.about
+        username = f"@{user_info.users[0].username}"
+    about = "None" if user_info.full_user.about is None else user_info.full_user.about
     user_info = f"""|=<b>Username: {username}
-|-Id: <code>{user_info.user.id}</code>
-|-Bot: <code>{user_info.user.bot}</code>
-|-Scam: <code>{user_info.user.scam}</code>
-|-Name: <code>{user_info.user.first_name}</code>
-|-Deleted: <code>{user_info.user.deleted}</code>
+|-Id: <code>{user_info.users[0].id}</code>
+|-Bot: <code>{user_info.users[0].bot}</code>
+|-Scam: <code>{user_info.users[0].scam}</code>
+|-Name: <code>{user_info.users[0].first_name}</code>
+|-Deleted: <code>{user_info.users[0].deleted}</code>
 |-BIO: <code>{about}</code>
 </b>"""
     await message.edit(user_info)
 
 
 @Client.on_message(filters.command("inffull", prefix) & filters.me)
-async def get_full_user_inf(client: Client, message: Message):
+async def get_full_user_inf(client, message):
     await message.edit("<code>Receiving the information...</code>")
     if len(message.text.split()) >= 2:
         try:
@@ -61,11 +61,10 @@ async def get_full_user_inf(client: Client, message: Message):
         except:
             user = message.from_user.id
     try:
-        await client.send_message("@creationdatebot", '/start')
-        await asyncio.sleep(1)
-        date_dict.clear()
         msg = await client.send_message("@creationdatebot", f"/id {user}")
         await asyncio.sleep(1)
+        date_dict = await client.get_history("@creationdatebot")
+        date_dict = date_dict[0].text
         await client.send(
             functions.messages.DeleteHistory(
                 peer=await client.resolve_peer(747653812), max_id=msg.chat.id
@@ -74,33 +73,31 @@ async def get_full_user_inf(client: Client, message: Message):
         user_info = await client.send(
             functions.users.GetFullUser(id=await client.resolve_peer(user))
         )
-        if user_info.user.username is None:
+        if user_info.users[0].username is None:
             username = "None"
         else:
-            username = f"@{user_info.user.username}"
-        about = "None" if user_info.about is None else user_info.about
+            username = f"@{user_info.users[0].username}"
+        about = "None" if user_info.full_user.about is None else user_info.full_user.about
         user_info = f"""|=<b>Username: {username}
-|-Id: <code>{user_info.user.id}</code>
-|-Account creation date: <code>{date_dict.get('date')}</code>
-|-Bot: <code>{user_info.user.bot}</code>
-|-Scam: <code>{user_info.user.scam}</code>
-|-Name: <code>{user_info.user.first_name}</code>
-|-Deleted: <code>{user_info.user.deleted}</code>
+|-Id: <code>{user_info.users[0].id}</code>
+|-Account creation date: <code>{date_dict}</code>
+|-Bot: <code>{user_info.users[0].bot}</code>
+|-Scam: <code>{user_info.users[0].scam}</code>
+|-Name: <code>{user_info.users[0].first_name}</code>
+|-Deleted: <code>{user_info.users[0].deleted}</code>
 |-BIO: <code>{about}</code>
-|-Contact: <code>{user_info.user.contact}</code>
-|-Can pin message: <code>{user_info.can_pin_message}</code>
-|-Mutual contact: <code>{user_info.user.mutual_contact}</code>
-|-Access hash: <code>{user_info.user.access_hash}</code>
-|-Restricted: <code>{user_info.user.restricted}</code>
-|-Verified: <code>{user_info.user.verified}</code>
-|-Phone calls available: <code>{user_info.phone_calls_available}</code>
-|-Phone calls private: <code>{user_info.phone_calls_private}</code>
-|-Blocked: <code>{user_info.blocked}</code></b>"""
-        date_dict.clear()
+|-Contact: <code>{user_info.users[0].contact}</code>
+|-Can pin message: <code>{user_info.full_user.can_pin_message}</code>
+|-Mutual contact: <code>{user_info.users[0].mutual_contact}</code>
+|-Access hash: <code>{user_info.users[0].access_hash}</code>
+|-Restricted: <code>{user_info.users[0].restricted}</code>
+|-Verified: <code>{user_info.users[0].verified}</code>
+|-Phone calls available: <code>{user_info.full_user.phone_calls_available}</code>
+|-Phone calls private: <code>{user_info.full_user.phone_calls_private}</code>
+|-Blocked: <code>{user_info.full_user.blocked}</code></b>"""
         await message.edit(user_info)
     except:
-        await message.edit("<code>An error has occurred...</code>")
-
+        await msg.edit("An error has occurred...")
 
 modules_help.append(
     {
