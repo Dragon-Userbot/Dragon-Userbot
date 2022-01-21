8dcaa498cbbd8a0d935648640b6c27fe1e6de69d
fuccsoc <fuccsoc@gmail.com>
2022-01-22 00:45:35 +0300

    Black style main.py & @iamnalinor's changes adoptation
    Co-Authored-By: Albert <63642425+iamnalinor@users.noreply.github.com>

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ app = Client("my_account")
 if __name__ == "__main__":
     app.start()
     for path in sorted((Path("plugins")).rglob("*.py")):
-        module_path = '.'.join(path.parent.parts + (path.stem,))
+        module_path = ".".join(path.parent.parts + (path.stem,))
         try:
             module = import_module(module_path)
             for name in vars(module).keys():
@@ -37,28 +37,33 @@ if __name__ == "__main__":
                     for handler, group in getattr(module, name).handlers:
                         try:
                             app.add_handler(handler, group)
-                            print('Successfully imported ' + module_path + ".")
+                            print("Successfully imported " + module_path + ".")
                         except:
-                            print('Cant add ' + module_path + " to handlers.")
+                            print("Cant add " + module_path + " to handlers.")
                 except AttributeError:
                     pass
         except:
-            print('Module ' + module_path + " throwed an error.")
+            print("Module " + module_path + " throwed an error.")
             print(sys.exc_info())
 
     if len(sys.argv) == 4:
         try:
             restart_type = sys.argv[3]
             if restart_type == "1":
-                text = "<code>Update process completed!</code>"
+                text = "<b>Update process completed!</b>"
             else:
-                text = "<code>Restart completed!</code>"
+                text = "<b>Restart completed!</b>"
             app.send_message(
-                chat_id=sys.argv[1], text=text, reply_to_message_id=int(sys.argv[2])
+                chat_id=sys.argv[1], 
+                text=text, 
+                reply_to_message_id=int(sys.argv[2])
             )
-        except:
+        except errors.RPCError:
             app.send_message(chat_id=sys.argv[1], text=text)
     auths = app.send(GetAuthorizations())["authorizations"]
     auth_hashes = [auth["hash"] for auth in auths]
     db.set("core.sessionkiller", "auths_hashes", auth_hashes)
+
+    print("Dragon-Userbot started!")
+
     idle()
