e0bcaa731bc96b368872a0b07c48ee3f35bf588f
Albert <63642425+iamnalinor@users.noreply.github.com>
2022-01-23 23:35:41 +0400

    fix purge.py

--- modules/purge.py
+++ modules/purge.py
@@ -14,10 +14,9 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message
-
-import asyncio
 
 from utils.misc import modules_help, prefix
 from utils.scripts import with_reply
@@ -30,21 +29,23 @@ async def del_msg(_, message: Message):
 
 
 @Client.on_message(filters.command("purge", prefix) & filters.me)
+@with_reply
 async def purge(client: Client, message: Message):
-    messages_to_purge = []
-    if message.reply_to_message:
-        async for msg in client.iter_history(
-            chat_id=message.chat.id,
-            offset_id=message.reply_to_message.message_id,
-            reverse=True,
-        ):
-            messages_to_purge.append(msg.message_id)
-    not_deleted_messages = []
-    for msgs in [
-        messages_to_purge[i : i + 100] for i in range(0, len(messages_to_purge), 100)
-    ]:
-        res = await client.delete_messages(message.chat.id, msgs)
-        await asyncio.sleep(1)
+    chunk = []
+    async for msg in client.iter_history(
+        chat_id=message.chat.id,
+        offset_id=message.reply_to_message.message_id,
+        reverse=True,
+    ):
+        chunk.append(msg.message_id)
+        if len(chunk) >= 100:
+            await client.delete_messages(message.chat.id, chunk)
+            chunk = []
+            await asyncio.sleep(1)
+
+    if len(chunk) > 0:
+        await client.delete_messages(message.chat.id, chunk)
+
 
 modules_help["purge"] = {
     "purge [reply]": "Purge (delete all messages) chat from replied message to last",
