0e6ec1b1713ee51822b273c97c66c2610dd12d88
asteroidden <49994705+asteroidden@users.noreply.github.com>
2022-02-10 16:25:48 +0200

    Naming fixes

--- modules/clear_notifs.py
+++ modules/clear_notifs.py
@@ -8,59 +8,59 @@ from utils.misc import modules_help, prefix
 
 @Client.on_message(filters.command(["clear_@"], prefix) & filters.me)
 async def solo_mention_clear(client: Client, message: Message):
-    await m.delete()
-    peer = await c.resolve_peer(m.chat.id)
+    await message.delete()
+    peer = await client.resolve_peer(message.chat.id)
     request = functions.messages.ReadMentions(peer=peer)
-    await c.send(request)
+    await client.send(request)
 
 
 @Client.on_message(filters.command(["clear_all_@"], prefix) & filters.me)
 async def global_mention_clear(client: Client, message: Message):
     request = functions.messages.GetAllChats(except_ids=[])
     try:
-        result = await c.send(request)
+        result = await client.send(request)
     except FloodWait as e:
-        await m.edit_text(
+        await message.edit_text(
             f"<code>FloodWait received. Wait {e.x} seconds before trying again</code>"
         )
         return
-    await m.delete()
+    await message.delete()
     for chat in result.chats:
         if type(chat) is types.Chat:
             peer_id = -chat.id
         elif type(chat) is types.Channel:
             peer_id = int(f"-100{chat.id}")
-        peer = await c.resolve_peer(peer_id)
+        peer = await client.resolve_peer(peer_id)
         request = functions.messages.ReadMentions(peer=peer)
-        await c.send(request)
+        await client.send(request)
 
 @Client.on_message(filters.command(["clear_reacts"], prefix) & filters.me)
 async def solo_reaction_clear(client: Client, message: Message):
-    await m.delete()
-    peer = await c.resolve_peer(m.chat.id)
+    await message.delete()
+    peer = await client.resolve_peer(message.chat.id)
     request = functions.messages.ReadReactions(peer=peer)
-    await c.send(request)
+    await client.send(request)
     
 
 @Client.on_message(filters.command(["clear_all_reacts"], prefix) & filters.me)
 async def global_reaction_clear(client: Client, message: Message):
     request = functions.messages.GetAllChats(except_ids=[])
     try:
-        result = await c.send(request)
+        result = await client.send(request)
     except FloodWait as e:
-        await m.edit_text(
+        await message.edit_text(
             f"<code>FloodWait received. Wait {e.x} seconds before trying again</code>"
         )
         return
-    await m.delete()
+    await message.delete()
     for chat in result.chats:
         if type(chat) is types.Chat:
             peer_id = -chat.id
         elif type(chat) is types.Channel:
             peer_id = int(f"-100{chat.id}")
-        peer = await c.resolve_peer(peer_id)
+        peer = await client.resolve_peer(peer_id)
         request = functions.messages.ReadReactions(peer=peer)
-        await c.send(request)
+        await client.send(request)
 
     
 modules_help["clear_notifs"] = {
