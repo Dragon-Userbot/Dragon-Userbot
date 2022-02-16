4e02e874981c874549972bf825b0f8015fb8ebb9
Albert <63642425+iamnalinor@users.noreply.github.com>
2022-02-16 09:57:33 +0400

    Black style and update check-black workflow

--- modules/python.py
+++ modules/python.py
@@ -22,6 +22,7 @@ from pyrogram.types import Message
 
 from utils.misc import modules_help, prefix
 from utils.scripts import format_exc
+
 # noinspection PyUnresolvedReferences
 from utils.db import db
 
