20fed8aaf88259f6d1f718fa3a6eb5017c53c602
John <60581076+john-phonk@users.noreply.github.com>
2022-04-05 16:51:08 +0300

    Update python.py
    black

--- modules/python.py
+++ modules/python.py
@@ -84,21 +84,20 @@ def user_eval(client: Client, message: Message):
         message.edit(format_exc(e))
 
 
-async_eval = import_library('async-eval')
+async_eval = import_library("async-eval")
 aeval = async_eval.eval
 
 
 async def aexec(code):
-    code = f'async def __todo(): ' + \
-           ''.join(f'\n {_l}' for _l in code.split('\n'))
-    if 'return' in code:
+    code = f"async def __todo(): " + "".join(f"\n {_l}" for _l in code.split("\n"))
+    if "return" in code:
         exec(code)
-        return await locals()['__todo']()
+        return await locals()["__todo"]()
     else:
         f = StringIO()
         exec(code)
         with redirect_stdout(f):
-            await locals()['__todo']()
+            await locals()["__todo"]()
         s = f.getvalue()
         return s
 
@@ -106,37 +105,41 @@ async def aexec(code):
 @Client.on_message(filters.command(["aex", "aexec"], prefix) & filters.me)
 async def aexec_handler(client: Client, message: Message):
     try:
-    	code = message.text.split(maxsplit=1)[1]
+        code = message.text.split(maxsplit=1)[1]
     except:
-    	code = ''
+        code = ""
     if not code:
-        return await message.edit('<b>Not found code to execute.</b>')
+        return await message.edit("<b>Not found code to execute.</b>")
     try:
         await message.edit("<b>Executing...</b>")
         s = await aexec(code)
-        s = s.replace('<', '').replace('>', '') if s else ''
-        return await message.edit(f'<b>Code:</b>\n<code>{code.replace("<", "").replace(">", "")}</code>\n\n<b>Result'
-                                  f':</b>\n<code>{s}</code>')
+        s = s.replace("<", "").replace(">", "") if s else ""
+        return await message.edit(
+            f'<b>Code:</b>\n<code>{code.replace("<", "").replace(">", "")}</code>\n\n<b>Result'
+            f":</b>\n<code>{s}</code>"
+        )
     except Exception as ex:
-        return await message.edit(f'<b>Error:</b>\n<code>{format_exc(ex)}</code>')
+        return await message.edit(f"<b>Error:</b>\n<code>{format_exc(ex)}</code>")
 
 
 @Client.on_message(filters.command(["aev", "aeval"], prefix) & filters.me)
 async def aeval_handler(client: Client, message: Message):
     try:
-    	code = message.text.split(maxsplit=1)[1]
+        code = message.text.split(maxsplit=1)[1]
     except:
-    	code = ''
+        code = ""
     if not code:
-        return await message.edit('<b>Not found expression.</b>')
+        return await message.edit("<b>Not found expression.</b>")
     try:
         await message.edit("<b>Executing...</b>")
         s = aeval(code)
-        s = s.replace('<', '').replace('>', '') if type(s) == str else s
-        return await message.edit(f'<b>Expression:</b>\n<code>{code.replace("<", "").replace(">", "")}</code>\n\n<b>Result'
-                                  f':</b>\n<code>{s}</code>')
+        s = s.replace("<", "").replace(">", "") if type(s) == str else s
+        return await message.edit(
+            f'<b>Expression:</b>\n<code>{code.replace("<", "").replace(">", "")}</code>\n\n<b>Result'
+            f":</b>\n<code>{s}</code>"
+        )
     except Exception as ex:
-        return await message.edit(f'<b>Error:</b>\n<code>{format_exc(ex)}</code>')
+        return await message.edit(f"<b>Error:</b>\n<code>{format_exc(ex)}</code>")
 
 
 modules_help["python"] = {
