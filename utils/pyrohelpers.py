b4c37a966438bd00272674855afcf88020c3174d
Muhammad Sena Al-Kahfi <msenakahfi14@gmail.com>
2022-07-29 18:16:56 +0700

    Create pyrohelpers.py

---
+++ utils/pyrohelpers.py
@@ -0,0 +1,39 @@
+import shlex
+from pyrogram import Client
+from pyrogram.types import Message
+
+
+def get_arg(message):
+    msg = message.text
+    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
+    split = msg[1:].replace("\n", " \n").split(" ")
+    if " ".join(split[1:]).strip() == "":
+        return ""
+    return " ".join(split[1:])
+
+
+def get_args(message):
+    try:
+        message = message.text
+    except AttributeError:
+        pass
+    if not message:
+        return False
+    message = message.split(maxsplit=1)
+    if len(message) <= 1:
+        return []
+    message = message[1]
+    try:
+        split = shlex.split(message)
+    except ValueError:
+        return message  # Cannot split, let's assume that it's just one long message
+    return list(filter(lambda x: len(x) > 0, split))
+
+
+def ReplyCheck(message: Message):
+    reply_id = None
+    if message.reply_to_message:
+        reply_id = message.reply_to_message.message_id
+    elif not message.from_user.is_self:
+        reply_id = message.message_id
+    return reply_id
