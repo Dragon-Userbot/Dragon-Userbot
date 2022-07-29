eb41ab0ff9d98f7bf4eded07a2cbed6ee43f7c45
Muhammad Sena Al-Kahfi <msenakahfi14@gmail.com>
2022-07-29 18:51:35 +0700

    Update purge.py

--- modules/purge.py
+++ modules/purge.py
@@ -21,32 +21,106 @@ from pyrogram.types import Message
 from utils.misc import modules_help, prefix
 from utils.scripts import with_reply
 
-
-@Client.on_message(filters.command("del", prefix) & filters.me)
-async def del_msg(_, message: Message):
-    await message.delete()
-    await message.reply_to_message.delete()
+import asyncio
+from requests import get
+from datetime import datetime
+from inspect import getfullargspec
+from pyrogram.errors import RPCError
+from pyrogram import filters, enums
+from pyrogram.types import Message
 
 
 @Client.on_message(filters.command("purge", prefix) & filters.me)
-@with_reply
-async def purge(client: Client, message: Message):
-    chunk = []
-    async for msg in client.get_chat_history(
-        chat_id=message.chat.id,
-        offset_id=message.reply_to_message.id,
-    ):
-        chunk.append(msg.id)
-        if len(chunk) >= 100:
-            await client.delete_messages(message.chat.id, chunk)
-            chunk = []
-            await asyncio.sleep(1)
+async def purge_message(client: Client, message): Message:
+    try:
+        if message.reply_to_message:
+            start = datetime.now()
+            y = await client.get_messages(
+                                message.chat.id,
+                                range(message.reply_to_message.id, message.id),
+                                replies=0
+                        )
+            msg_id = []
+            msg_id.clear()
+            for msg in y:
+                 msg_id.append(msg.id)
+            await app.delete_messages(
+                                message.chat.id,
+                                msg_id
+                        )
+            sec = (datetime.now() - start).seconds
+            await message.edit(f"Deleted `{len(msg_id)}` messages in `{sec}` seconds.")
+            await asyncio.sleep(5)
+            await message.delete()
+        else:
+            await message.edit("Reply to message you want to delete from.")
+    except RPCError:
+        await message.edit("I can't do that.")
 
-    if len(chunk) > 0:
-        await client.delete_messages(message.chat.id, chunk)
+
+@Client.on_message(filters.command("del", prefix) & filters.me)
+async def delete_replied(client: Client, message: Message):
+    msg_ids = [message.id]
+    if message.reply_to_message:
+        msg_ids.append(message.reply_to_message.id)
+    await app.delete_messages(message.chat.id, msg_ids)
+
+
+@Client.on_message(filters.command("purgeme", prefix) & filters.me)
+async def purge_me(client: Client, message: Message):
+    if len(message.command) != 2:
+        return
+    n = message.text.split(None, 1)[1].strip()
+    if not n.isnumeric():
+        return await message.edit("Give me a number not a strings.")
+    n = int(n)
+    if n < 1:
+        return await message.edit("Give me a number total message you want to delete.")
+    chat_id = message.chat.id
+    message_ids = [
+        m.id
+        async for m in client.search_messages(
+            chat_id,
+            from_user="me",
+            limit=n,
+        )
+    ]
+    if not message_ids:
+        return await message.edit("Pesan tidak di temukan")
+    to_delete = [message_ids[i : i + 99] for i in range(0, len(message_ids), 99)]
+    for hundred_messages_or_less in to_delete:
+        await client.delete_messages(
+            chat_id=chat_id,
+            message_ids=hundred_messages_or_less,
+            revoke=True,
+        )
+    ms_g = await client.send_message(
+        message.chat.id,
+        f"Successfully delete <code>{n}</code> message.",
+    )
+    await asyncio.sleep(5)
+    await ms_g.delete()
+
+
+@Client.on_message(filters.command("purgeall", prefix) & filters.me)
+async def purge_all(client: Client, message: Message):
+    y = message.reply_to_message
+    if not y:
+        await message.edit("reply to a user message or to your message to start purgeall.")
+        return
+    else:
+        try:
+            turok = y.from_user.id
+            bisa = await client.get_users(turok)
+            await message.edit("Starting delete messages.")
+            await client.delete_user_history(message.chat.id, bisa.id)
+        except RPCError:
+            await message.edit("Sorry you're not admin.")
 
 
 modules_help["purge"] = {
     "purge [reply]": "Purge (delete all messages) chat from replied message to last",
     "del [reply]": "Delete replied message",
+    "purgeme [total_msg]": "Delete your own messages with specify total messages",
+    "purgeall [reply]": "[Be carefull use this] Delete all your message or your replied message",
 }
