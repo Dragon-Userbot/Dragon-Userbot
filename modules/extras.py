f0cd9de7d3895c9017aef97190b1c0605b36a620
Muhammad Sena Al-Kahfi <msenakahfi14@gmail.com>
2022-07-29 22:24:01 +0700

    Update extras.py

--- modules/extras.py
+++ modules/extras.py
@@ -27,10 +27,10 @@ async def sosmed(client, message):
             await y.delete()
     async for turok in client.search_messages(bot, filter=enums.MessagesFilter.VIDEO, limit=1):
         await client.send_video(chat, video=turok.video.file_id, caption=f"**Upload by:** [{pop}](tg://user?id={ah})")
-        y = client.search_messages(bot, query="Please", limit=1)
-        await y.delete()
         await uh.delete()
         await turok.delete()
+    async for konol in client.search_messages(bot, query="Please", limit=1)
+        await konol.delete()
 
 
 modules_help["extras"] = {
