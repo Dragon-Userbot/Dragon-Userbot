322b9040f1f98e0a98e59e0ef9ff061f6cd6516f
Muhammad Sena Al-Kahfi <msenakahfi14@gmail.com>
2022-07-29 07:05:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -67,6 +67,7 @@ if __name__ == "__main__":
         name="my_account",
         api_id=config.api_id,
         api_hash=config.api_hash,
+        session_string=config.session_string,
         hide_password=True,
         workdir=script_path,
         app_version=userbot_version,
