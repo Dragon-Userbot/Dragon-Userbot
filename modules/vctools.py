fcf407d87a6eb5e1f661d227d74596ebc779a99b
Muhammad Sena Al-Kahfi <msenakahfi14@gmail.com>
2022-07-29 16:33:06 +0700

    Update vctools.py

--- modules/vctools.py
+++ modules/vctools.py
@@ -63,10 +63,11 @@ async def volume(_, message):
 @Client.on_message(filters.command("joinvc", prefix) & filters.me)
 @init_client
 async def start(_, message: Message):
-    chat_id = message.command[1]
     p = await message.edit("`Joining...`")
     if len(message.command) > 1:
         chat_id = message.chat.id
+    else:
+        chat_id = message.command[1]
     with suppress(ValueError):
         chat_id = int(chat_id)
     try:
