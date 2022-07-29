61354730acb423a715c4e052bab0e5379c53de32
Muhammad Sena Al-Kahfi <msenakahfi14@gmail.com>
2022-07-29 17:57:20 +0700

    Update decorators.py

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,12 +1,19 @@
 import traceback
 from typing import Union
 from functools import wraps
-from utils.bot.set_inline import set_inline_in_botfather
 from pyrogram.types import Message, InlineQuery, CallbackQuery
 from pyrogram import Client, StopPropagation, ContinuePropagation
 from pyrogram.errors import (
     MessageEmpty, MessageIdInvalid, BotInlineDisabled, MessageNotModified,
     UserNotParticipant)
+from main import bot
+import asyncio
+
+
+async def set_inline_in_botfather(Client: client):
+    message = await Client.send_message("botfather", "/setinline")
+    await asyncio.sleep(1)
+    await message.reply(f"@{bot.username}")
 
 
 def inline_check(func):
