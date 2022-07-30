65d38caf0b611e2ce8bb75d16cb437a2c7932e5a
Muhammad Sena Al-Kahfi <msenakahfi14@gmail.com>
2022-07-30 10:38:38 +0700

    Update broadcast.py

--- modules/broadcast.py
+++ modules/broadcast.py
@@ -3,63 +3,119 @@ from pyrogram import Client, filters, enums
 from requests import get
 from utils.misc import modules_help, prefix
 from utils.pyrohelpers import get_arg
+from pyrogram.errors import FloodWait
+from pyrogram.types import Message
 
 
 @Client.on_message(filters.command("gcast", prefix) & filters.me)
-async def chat_broadcast(client, message):
-    if message.reply_to_message:
-        msg = message.reply_to_message
+async def gcast(client: Client, message: Message):
+    if not message.reply_to_message:
+        pass
     else:
-        msg = get_arg(message)
-    if not msg:
-        return await message.edit_text("Give me a Text/Reply to a message to broadcast it")
+        msg = message.reply_to_message
+        yanto = await message.reply_text("`Global Broadcasting!`")
+        sent = 0
+        failed = 0
+        async for dialog in client.iter_dialogs():
+            chat_type = dialog.chat.type
+            if chat_type in [
+                "supergroup",
+                "group",
+            ]:
+                chat = dialog.chat.id
+                if chat not in GCAST_BLACKLIST and chat not in BLACKLIST_GCAST:
+                    try:
+                        await msg.copy(chat)
+                        sent = sent + 1
+                        await asyncio.sleep(0.1)
+                    except:
+                        failed = failed + 1
+                        await asyncio.sleep(0.1)
+        return await yanto.edit_text(
+            f"Done in {sent} chats, error in {failed} chat(s)"
+        )
+        return
+    if len(message.command) < 2:
+        await message.reply_text(
+             "**usage**:\n.gcast <text> or reply to message"
+        )
+        return
+    yanto = await message.reply_text("`Global Broadcasting!`")
+    panjul = message.text.split(None, 1)[1]
     sent = 0
     failed = 0
-    async for dialog in client.get_dialogs():
+    async for dialog in client.iter_dialogs():
         chat_type = dialog.chat.type
         if chat_type in [
-            enums.ChatType.GROUP,
-            enums.ChatType.SUPERGROUP,
+             "supergroup",
+             "group",
         ]:
-            chat = dialog.chat.id
-            try:
-                await msg.copy(chat)
-                sent = sent + 1
-                await asyncio.sleep(0.1)
-            except:
-                failed = failed + 1
-                await asyncio.sleep(0.1)
-    await message.edit_text(
-        f"**Broadcast done!\n\nSucces in:** `{sent}` **Chats\nFailed in:** `{failed}` **Chats**"
+             chat = dialog.chat.id
+             if chat not in GCAST_BLACKLIST and chat not in BLACKLIST_GCAST:
+                 try:
+                     await client.send_message(chat, text=panjul)
+                     sent = sent + 1
+                     await asyncio.sleep(0.1)
+                 except:
+                     failed = failed + 1
+                     await asyncio.sleep(0.1)
+    return await yanto.edit_text(
+        f"Done in {sent} chats, error in {failed} chat(s)"
     )
 
 
 @Client.on_message(filters.command("gucast", prefix) & filters.me)
-async def chat_broadcast(client, message):
-    if message.reply_to_message:
-        msg = message.reply_to_message
+async def gucast(client: Client, message: Message):
+    if not message.reply_to_message:
+        pass
     else:
-        msg = get_arg(message)
-    if not msg:
-        return await message.edit_text("Give me a Text/Reply to a message to broadcast it")
+        msg = message.reply_to_message
+        yanto = await message.reply_text("`Global Broadcasting to users!`")
+        sent = 0
+        failed = 0
+        async for dialog in client.iter_dialogs():
+            chat_type = dialog.chat.type
+            if chat_type in [
+                "private",
+            ]:
+                chat = dialog.chat.id
+                try:
+                    await msg.copy(chat)
+                    sent = sent + 1
+                    await asyncio.sleep(0.1)
+                except:
+                    failed = failed + 1
+                    await asyncio.sleep(0.1)
+                await yanto.edit_text(
+                    f"✅ **Gucast Successfully\nSend to:** {sent} **Chats\n Failed to send :** {failed} **Chats**"
+                )
+        return
+    if len(message.command) < 2:
+        await message.reply_text(
+             "**usage**:\n.gucast <text> or reply to message"
+        )
+        return
+    yanto = await message.reply_text("`Global Broadcasting to users!`")
+    panjul = message.text.split(None, 1)[1]
     sent = 0
     failed = 0
-    async for dialog in client.get_dialogs():
+    async for dialog in client.iter_dialogs():
         chat_type = dialog.chat.type
         if chat_type in [
-            enums.ChatType.PRIVATE
-            ]:
-            chat = dialog.chat.id
-            try:
-                await msg.copy(chat)
-                sent = sent + 1
-                await asyncio.sleep(0.1)
-            except:
-                failed = failed + 1
-                await asyncio.sleep(0.1)
-    await message.edit_text(
-        f"**Broadcast Users done!\n\nSucces to:** `{sent}` **Chats\nFailed to:** `{failed}` **Chats**"
-    )
+             "private",
+        ]:
+             chat = dialog.chat.id
+             try:
+                 await client.send_message(chat, text=panjul)
+                 sent = sent + 1
+                 await asyncio.sleep(0.1)
+             except:
+                 failed = failed + 1
+                 await asyncio.sleep(0.1)
+             await yanto.edit_text(
+                 f"**✅ Gucast Successfully\nSend to:** {sent} **Chats\n Failed to send :** {failed} **Chats**"
+             )
+
 
 modules_help["broadcast"] = {
     "gcast [input/reply]": "To broadcasting a message to your all groups",
